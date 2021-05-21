c43ed2727cee9ad660bb9a6f9dd95143eba1f6be
Ahmed Ashraf <ahmedashraf@aucegypt.edu>
2021-05-21 18:29:36 +0200

    First commit

---
+++ generate.py
@@ -0,0 +1,20 @@
+import qrcode
+
+def generate(text,image,size):
+	qr = qrcode.QRCode(
+		version=1,
+		error_correction=qrcode.constants.ERROR_CORRECT_H,
+		box_size=size,
+		border=5,
+	)
+	qr.add_data(text)
+	qr.make(fit=True)
+	img =qr.make_image(fill_color="black",back_color="white").convert('RGB')
+	
+	if image is not None:
+		logo_display = image
+		logo_display.thumbnail((60, 60))
+		logo_pos = ((img.size[0]- logo_display.size[0])// 2, (img.size[1] -logo_display.size[1]) //2)
+		img.paste(logo_display,logo_pos)
+		
+	return img
